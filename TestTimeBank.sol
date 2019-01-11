f0198f051b8cd39d9721883c6f986978db20de2f
wstenzel <wills.stenzel@gmail.com>
2019-01-11 16:53:34 -0500

    removed hard coded address

--- TestTimeBank.sol
+++ TestTimeBank.sol
@@ -2,14 +2,13 @@ pragma solidity 0.4.25;
 
 import "./TimeBank.sol";
 
-contract TestTimeBank {
+contract TestTimeBank is TimeBank {
 
   function deposit() public payable {}
 
   function test_depositFunds() public {
-    TimeBank bank = TimeBank(0x64910A4599344D0062AF2B1719138f2F957100D2);
+    TimeBank bank = TimeBank(address(this));
     bank.depositFunds.value(1000)(1647224721);
-
 
   // Not possible to test withdraw funds becasue the test case cannot be
   // timed correctly so no error will be thrown
