ba9f5af1a978998bb3d35930f083e618fe4d4cc1
wstenzel <wills.stenzel@gmail.com>
2019-01-14 12:34:43 -0500

    updated tests

--- TestTimeBank.sol
+++ TestTimeBank.sol
@@ -10,7 +10,17 @@ contract TestTimeBank is TimeBank {
     TimeBank bank = TimeBank(address(this));
     bank.depositFunds.value(1000)(1647224721);
 
-  // Not possible to test withdraw funds becasue the test case cannot be
-  // timed correctly so no error will be thrown
+    uint fundsDeposited;
+    uint withdrawTime;
+    uint blockTime;
+    (fundsDeposited, withdrawTime, blockTime) = bank.getInfo();
+
+    assert(fundsDeposited == 1000);
+    assert(withdrawTime == 1647224721);
+    // cannot test blockTime because it is continually changing
+
+    // tests that we are unable to withdrawFunds right after depositing them
+    bool result = address(this).call(bytes4(keccak256("withdraw()")));
+    assert(result == false);
  }
 }
